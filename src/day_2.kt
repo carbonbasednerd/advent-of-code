4aaec76a58196d2d83a1d5085149cecff349e3be
jay <carbonbasednerd@gmail.com>
2020-12-02 12:56:25 -0500

    day 2 part 2

--- src/day_2.kt
+++ src/day_2.kt
@@ -11,19 +11,35 @@ fun readAndFormat(fileName: String): List<Triple<String, String, String>> {
 }
 
 fun part1PasswordCheck(data: List<Triple<String, String, String>>): Int {
-    var passwordValidCounter = 0
+    var validPasswordCounter = 0
     data.forEach { passwordData ->
         val counter = passwordData.third.count { it == passwordData.second[0]}
         val range = passwordData.first.split("-")
         if (counter in (range[0].toInt() .. range[1].toInt())){
-            passwordValidCounter++
+            validPasswordCounter++
         }
     }
-    return passwordValidCounter
+    return validPasswordCounter
 }
 
 fun part2PasswordCheck(data: List<Triple<String, String, String>>): Int {
-    val validPasswordCounter = 0
+    var validPasswordCounter = 0
+    data.forEach { passwordData ->
+        val indexes = passwordData.first.split("-")
+        val passwordLength = passwordData.third.length
+        var validPassword = false
+        if (indexes[0].toInt() <= passwordLength && passwordData.third[indexes[0].toInt()-1] == passwordData.second[0]) {
+            validPassword = !validPassword
+        }
+        if (indexes[1].toInt() <= passwordLength && passwordData.third[indexes[1].toInt()-1] == passwordData.second[0]) {
+            validPassword = !validPassword
+        }
+
+        if (validPassword) {
+            validPasswordCounter++
+        }
+
+    }
     return validPasswordCounter
 }
 
