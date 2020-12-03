a1971b30ed6fabe963fd34241a6a2f940ca75d36
jay <carbonbasednerd@gmail.com>
2020-12-03 09:40:08 -0500

    day 3 part 2

--- src/day_3.kt
+++ src/day_3.kt
@@ -1,30 +1,44 @@
 import java.io.File
 
-val HORIZONTAL_BASE_MOVE = 3
+var horizontalBaseMove = 3
+var verticalBaseMove = 1
 
 fun readTreeMap(fileName: String): List<String> {
     return File(fileName).readLines()
 }
 
 fun calculateNextIndex(currentCount: Int, queueLength: Int): Int {
-    val nextMove = currentCount + HORIZONTAL_BASE_MOVE
+    val nextMove = currentCount + horizontalBaseMove
     return if (queueLength - nextMove > 0) nextMove else (queueLength - nextMove) * -1
 }
 
-fun forestThroughTrees1(data: List<String>) {
+fun forestThroughTrees1(data: List<String>): Int {
     val queueLength = data[0].length
     var mutatingIndex = 0
     var treeCollisions = 0
-    data.drop(1).forEach {
+    for (i in 0 until data.size step verticalBaseMove) {
         mutatingIndex = calculateNextIndex(mutatingIndex, queueLength)
-        if (it[mutatingIndex] == '#') {
+        if (data[i][mutatingIndex] == '#') {
             treeCollisions++
         }
     }
 
-    println("Ouch! You hit $treeCollisions trees!")
+    return treeCollisions
+}
+
+fun forestThroughTrees2(data: List<String>): Int {
+    val scenarios = listOf(1 to 1, 3 to 1, 5 to 1, 7 to 1, 1 to 2)
+    var treeCollisions = 1
+    scenarios.forEach {
+        horizontalBaseMove = it.first
+        verticalBaseMove = it.second
+        treeCollisions *= forestThroughTrees1(data.drop(verticalBaseMove))
+    }
+    
+    return treeCollisions
 }
 
 fun main() {
-    forestThroughTrees1(readTreeMap("data_day3"))
+    println("Ouch! You hit ${forestThroughTrees1(readTreeMap("data_day3").drop(verticalBaseMove))} trees!")
+    println("Ouch! You hit ${forestThroughTrees2(readTreeMap("data_day3"))} trees!")
 }
