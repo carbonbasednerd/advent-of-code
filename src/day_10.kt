ca8917d009d65776a373248d77bd70ce4fa39cc4
jay <carbonbasednerd@gmail.com>
2020-12-11 22:56:38 -0500

    completed day 10 part 1 and 2

--- src/day_10.kt
+++ src/day_10.kt
@@ -1,27 +1,25 @@
 import java.io.File
-
-val joltData = mutableListOf<Int>()
-val joltDataSorted = mutableListOf<Int>()
+val joltData = mutableListOf<Long>()
 
 fun readJolts(fileName: String) {
     File(fileName).forEachLine {
-        joltData.add(it.toInt())
+        joltData.add(it.toLong())
     }
 }
 
-fun testJoltAdapters(): Int {
+fun testJoltAdapters(): Long {
     val sortedJoltData = joltData.sorted()
-    var currentJolts = 0
-    var differenceOfOne = 0
-    var differenceOfThree = 1
+    var currentJolts = 0L
+    var differenceOfOne = 0L
+    var differenceOfThree = 1L
     for (jolt in sortedJoltData) {
         val joltDiff = jolt - currentJolts
-        if (joltDiff == 1) {
+        if (joltDiff == 1L) {
             currentJolts = jolt
             differenceOfOne++
-        } else if (joltDiff == 2) {
+        } else if (joltDiff == 2L) {
             currentJolts = jolt
-        } else if (joltDiff == 3) {
+        } else if (joltDiff == 3L) {
             currentJolts = jolt
             differenceOfThree++
         } else {
@@ -33,123 +31,17 @@ fun testJoltAdapters(): Int {
     return differenceOfOne * differenceOfThree
 }
 
-fun testCombinations(): Int {
-    val sortedJoltData = joltData.sorted()
-    var paths = 1
-    var index = 0
-    for (jolt in sortedJoltData) {
-        //look ahead and see if the next potential 3 values are there
-        val joltRange = (jolt + 1..jolt + 3)
-        val lookAheadList = mutableListOf<Int>()
-        var parseLastBit = false
-        var hasOutsideRange = false
-        for (x in (1..4)) {
-            if (sortedJoltData.size <= index+x) {
-                parseLastBit = true
-                break
-            } else if (sortedJoltData[index + x] in joltRange) {
-                lookAheadList.add(sortedJoltData[index + x])
-            } else {
-                hasOutsideRange = true
-                lookAheadList.add(sortedJoltData[index + x])
-                break
-            }
-        }
+fun testCombinations(): Long {
+    var sortedJoltData = joltData.sorted()
 
-        if (parseLastBit) {
-            when(lookAheadList.size) {
-                3 -> {
-                    when (lookAheadList[0] + 3) {
-                        lookAheadList[2] -> paths += 2
-                        else -> paths++
-                    }
-                }
-            }
-
-        } else {
-            when(lookAheadList.size) {
-                3 -> {
-                    when (lookAheadList[0]+3) {
-                        lookAheadList[2] -> paths += 3
-                        else -> paths++
-                    }
-                }
-                4 -> {
-                    when (lookAheadList[0]+3) {
-                        lookAheadList[3] -> {
-                            paths += 6
-                        }
-                        lookAheadList[3]-1 -> {
-                            paths += 5
-                        }
-                        lookAheadList[3]-2 -> {
-                            paths += 4
-                        }
-
-                    }
-                }
-            }
-        }
-        index++
-
-    }
-    return paths + 3
-}
-
-fun testCombinations3(start: Int): Int {
-    var counter = 0
-    for (x in (start..(joltDataSorted.size -1))) {
-        val joltRange = (joltDataSorted[x]+1..joltDataSorted[x]+3)
-        val branches = mutableListOf<Int>()
-        for (y in (1..3)) {
-            if (x+y >= joltDataSorted.size) break
-            if (joltDataSorted[x+y] in joltRange){
-                branches.add(x+y)
-//                counter += testCombinations3(x+y)
-            }
-        }
-        if (branches.size > 1) {
-            for (z in (1..(branches.size -1))) {
-                counter += testCombinations3(branches[z])
-            }
+    val children = mutableMapOf<Long, Long>()
+    val joltRange = (1..3)
+    for (x in (0..2)) {
+        if (x >= sortedJoltData.size) break
+        if (sortedJoltData[x] in joltRange){
+            children[sortedJoltData[x]] = 1
         }
     }
-
-    return counter+1
-}
-
-fun testCombinations2(): Int {
-    val sortedJoltData = joltData.sorted()
-    var paths = 1
-    var index = 0
-    var depth = 0
-    var branches = 1
-    for (jolt in sortedJoltData) {
-        //look ahead and see if the next potential 3 values are there
-        val joltRange = (jolt + 1..jolt + 3)
-        var count = 0
-        for (x in (1..3)) {
-            if (index + x >= sortedJoltData.size) break
-            if (sortedJoltData[index + x] in joltRange){
-                count++
-            }
-        }
-
-        if (count > 1) {
-            paths += count
-        }
-
-        index++
-        depth++
-    }
-
-    return paths
-}
-
-fun testCombinations4(): Int {
-    val sortedJoltData = joltData.sorted()
-    val children = mutableMapOf<Int, Int>()
-    children[sortedJoltData[0]] = 1
     sortedJoltData.forEachIndexed { index, i ->
         val joltRange = ( i+1..i+3)
         for (x in (1..3)) {
@@ -166,12 +58,9 @@ fun testCombinations4(): Int {
     return children.values.max()!!
 }
 
-
 fun main() {
-    readJolts("data/test_data_day10_1")
+    readJolts("data/data_day10")
     println("Result of jolt testing is ${testJoltAdapters()}")
-    println("Find all combinations ${testCombinations4()}")
-//    joltDataSorted.addAll(joltData.sorted())
-//    println("Number of permutations ${testCombinations2()}")
-//    println("Number of permutations ${testCombinations3(0)}")
+    println("Find all combinations ${testCombinations()}")
+
 }
