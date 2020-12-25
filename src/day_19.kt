dd15aa0078570e26445512f45b200511484c3abc
jay <carbonbasednerd@gmail.com>
2020-12-25 16:59:56 -0500

    complete day 19 part 1

--- src/day_19.kt
+++ src/day_19.kt
@@ -49,17 +49,64 @@ fun readRulesAndData(fileName: String) {
         }
     }
 }
-
-fun buildRules(ruleNumber: Int) {
-    // loop through and build all possible strings?
+// part one
+val ruleMap = mutableMapOf<Int, List<String>>()
+var depth = 0
+fun buildRules(ruleNumber: Int): List<String> {
+    println("Depth ${++depth}")
+    if (ruleMap.containsKey(ruleNumber)) {
+        return ruleMap[ruleNumber]!!
+    } else {
+        if (rulesData[ruleNumber] is EndRule) {
+            val tempList = listOf((rulesData[ruleNumber] as EndRule).value.toString())
+            ruleMap[ruleNumber] = tempList
+            return tempList
+        } else {
+            val branches = rulesData[ruleNumber] as BranchRule
+            val listOfAnds = mutableListOf<List<String>>()
+            branches.ands.forEach {
+                var possibleStrings = mutableListOf<String>()
+                it.forEach { i ->
+                    val returnedLists = buildRules(i)
+                    val newList = mutableListOf<String>()
+                    if (possibleStrings.isNotEmpty()) {
+                        possibleStrings.forEach { x ->
+                            returnedLists.forEach { y ->
+                                newList.add(x.plus(y))
+                            }
+                        }
+                    } else {
+                        newList.addAll(returnedLists)
+                    }
+                    possibleStrings = newList
+                }
+                listOfAnds.add(possibleStrings)
+            }
+            val flattenedList = listOfAnds.flatten()
+            ruleMap[ruleNumber] = flattenedList
+            return flattenedList
+        }
+    }
 }
 
-//read in data - break into classes for the pattern matching logic
-//read in test data as strings
-//build all possible patterns for a particular entry, save
-//loop through data and match exacts
 fun main() {
-    readRulesAndData("data/test_data_day19")
+    readRulesAndData("data/data_day19")
     println(rulesData)
     println(imageData)
+    println("building rules")
+    val possibleCombinations = buildRules(0)
+    var counter = 0
+    println("rules built")
+    imageData.forEach image@{data ->
+        println("checking image ${data}")
+        possibleCombinations.forEach {
+            if (data.length != it.length) return@image
+            if (data.matches("^${it}$".toRegex())) {
+                counter++
+                return@image
+            }
+        }
+
+    }
+    println("Found $counter matches for Rule 0")
 }
