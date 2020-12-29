2aedafbf046782ec9649f29e4a30ab8cf5e4805b
jay <carbonbasednerd@gmail.com>
2020-12-28 19:36:29 -0500

    completed part 1 of day 20

--- src/day_20.kt
+++ src/day_20.kt
@@ -2,41 +2,181 @@ import java.io.File
 
 val tiles = mutableListOf<Tile>()
 
-data class Tile(val tileNumber: Int, val tileLayout: List<List<Boolean>>)
+data class Tile(
+    val tileNumber: Int, val tileLayout: List<String>,
+    var top: String = "", var bottom: String = "", var right: String = "",
+    var left: String = ""
+) {
+    fun calcEdges() {
+        top = tileLayout.first()
+        bottom = tileLayout.last()
+        val leftBuilder = StringBuilder()
+        val rightBuilder = StringBuilder()
+        tileLayout.forEach {
+            leftBuilder.append(it.first())
+            rightBuilder.append(it.last())
+        }
+        left = leftBuilder.toString()
+        right = rightBuilder.toString()
+    }
+
+    fun topReversed(): Long = top.reversed().toLong()
+    fun bottomReversed(): Long = bottom.reversed().toLong()
+    fun leftReversed(): Long = left.reversed().toLong()
+    fun rightReversed(): Long = right.reversed().toLong()
+
+    fun topL(): Long = top.toLong()
+    fun bottomL(): Long = bottom.toLong()
+    fun leftL(): Long = left.toLong()
+    fun rightL(): Long = right.toLong()
+}
 
 fun readTiles(fileName: String) {
     var newTile = true
     var tempTileName: Int = 0
-    var tempLayout: MutableList<List<Boolean>> = mutableListOf()
+    var tempLayout: MutableList<String> = mutableListOf()
     File(fileName).forEachLine {
         if (newTile) {
             val header = it.split(" ")
-            tempTileName = header[1].replace(":","").toInt()
+            tempTileName = header[1].replace(":", "").toInt()
             tempLayout = mutableListOf()
             newTile = false
-        } else if(it == "") {
+        } else if (it == "") {
             newTile = true
             tiles.add(Tile(tempTileName, tempLayout))
         } else {
-            val listPoints = mutableListOf<Boolean>()
-            listPoints.addAll(it.map{p -> p == '.'})
-            tempLayout.add(listPoints)
+            val listPoints = StringBuilder()
+            it.forEach { p ->
+                if (p == '.') {
+                    listPoints.append('0')
+                } else {
+                    listPoints.append('1')
+                }
+            }
+            tempLayout.add(listPoints.toString())
         }
     }
 }
 
 //part one
-fun tilesAssemble() {
-    // create a tile grid? This way we can find the corners first.
-    // a top should have no matching border to it's top, bottom to bottom etc..
-    //so a corner would be defined as, top right no matching Top and right
-    //All we need is the four corners so, we don't need to figure out the rest
-    // we can figure out the grid size by dividing it by two (our data set is 144 tiles)
-    
+fun calculateEdges() {
+    tiles.forEach {
+        it.calcEdges()
+    }
+}
 
+fun tilesAssemble(): Long {
+    var answer = 1L
+    var tileCorners = mutableListOf<Int>()
+    tiles.forEach main@{ t ->
+        var bottom = false
+        var top = false
+        var right = false
+        var left = false
+        tiles.forEach sub@{
+            if (it.tileNumber == t.tileNumber) return@sub
+
+            //unmodified
+            if ((t.topL() xor it.bottomL()) == 0L) top = true
+            if ((t.bottomL() xor it.topL()) == 0L) bottom = true
+            if ((t.rightL() xor it.leftL()) == 0L) right = true
+            if ((t.leftL() xor it.rightL()) == 0L) left = true
+
+            //rotated 90
+            if ((t.topL() xor it.rightReversed()) == 0L) top = true
+            if ((t.bottomL() xor it.leftReversed()) == 0L) bottom = true
+            if ((t.rightL() xor it.bottomL()) == 0L) right = true
+            if ((t.leftL() xor it.topL()) == 0L) left = true
+
+            //rotated 180
+            if ((t.topL() xor it.topReversed()) == 0L) top = true
+            if ((t.bottomL() xor it.bottomReversed()) == 0L) bottom = true
+            if ((t.rightL() xor it.rightReversed()) == 0L) right = true
+            if ((t.leftL() xor it.leftReversed()) == 0L) left = true
+
+            //flipped left right
+            if ((t.topL() xor it.bottomReversed()) == 0L) top = true
+            if ((t.bottomL() xor it.topReversed()) == 0L) bottom = true
+            if ((t.rightL() xor it.rightL()) == 0L) right = true
+            if ((t.leftL() xor it.leftL()) == 0L) left = true
+
+            //flipped left right 90
+            if ((t.topL() xor it.leftReversed()) == 0L) top = true
+            if ((t.bottomL() xor it.rightReversed()) == 0L) bottom = true
+            if ((t.rightL() xor it.bottomReversed()) == 0L) right = true
+            if ((t.leftL() xor it.topReversed()) == 0L) left = true
+
+            //flipped left right 180
+            if ((t.topL() xor it.topL()) == 0L) top = true
+            if ((t.bottomL() xor it.bottomL()) == 0L) bottom = true
+            if ((t.rightL() xor it.leftReversed()) == 0L) right = true
+            if ((t.leftL() xor it.rightReversed()) == 0L) left = true
+
+            //flipped left right 270
+            if ((t.topL() xor it.rightL()) == 0L) top = true
+            if ((t.bottomL() xor it.leftL()) == 0L) bottom = true
+            if ((t.rightL() xor it.topL()) == 0L) right = true
+            if ((t.leftL() xor it.bottomL()) == 0L) left = true
+
+            //flipped top bottom
+            if ((t.topL() xor it.topReversed()) == 0L) top = true
+            if ((t.bottomL() xor it.bottomReversed()) == 0L) bottom = true
+            if ((t.rightL() xor it.leftL()) == 0L) right = true
+            if ((t.leftL() xor it.rightL()) == 0L) left = true
+
+            //flipped top bottom 90
+            if ((t.topL() xor it.rightReversed()) == 0L) top = true
+            if ((t.bottomL() xor it.leftReversed()) == 0L) bottom = true
+            if ((t.rightL() xor it.topReversed()) == 0L) right = true
+            if ((t.leftL() xor it.bottomReversed()) == 0L) left = true
+
+            //flipped top bottom 180
+            if ((t.topL() xor it.bottomL()) == 0L) top = true
+            if ((t.bottomL() xor it.topL()) == 0L) bottom = true
+            if ((t.rightL() xor it.rightReversed()) == 0L) right = true
+            if ((t.leftL() xor it.leftReversed()) == 0L) left = true
+
+            //flipped top bottom 270
+            if ((t.topL() xor it.leftL()) == 0L) top = true
+            if ((t.bottomL() xor it.rightL()) == 0L) bottom = true
+            if ((t.rightL() xor it.bottomL()) == 0L) right = true
+            if ((t.leftL() xor it.topL()) == 0L) left = true
+
+        }
+        if (top && left && !right && !bottom) {
+            answer*=t.tileNumber
+            tileCorners.add(t.tileNumber)
+            println("Tile: ${t.tileNumber}")
+        }
+        if (top && right && !left && !bottom) {
+            answer*=t.tileNumber
+            tileCorners.add(t.tileNumber)
+            println("Tile: ${t.tileNumber}")
+        }
+        if (bottom && left && !top && !right) {
+            answer*=t.tileNumber
+            tileCorners.add(t.tileNumber)
+            println("Tile: ${t.tileNumber}")
+        }
+        if (bottom && right && !top && !left) {
+            answer*=t.tileNumber
+            tileCorners.add(t.tileNumber)
+            println("Tile: ${t.tileNumber}")
+        }
+    }
+    return answer
+}
+
+//part 2 - my part one solution won't solve the solution. It will give me the corners
+// but that's it. I'll need to build the whole thing.
+fun imagesAssembleFull() {
+    
 }
 
 fun main() {
     readTiles("data/data_day20")
+    calculateEdges()
     println(tiles.count())
+    println(tiles)
+    println("All the corners = ${tilesAssemble()}")
 }
