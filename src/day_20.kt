1623546fb597f52fbf73e86b3ead5cabd4ebd0fb
Jay Raffi <carbonbasednerd@gmail.com>
2020-12-29 23:59:27 -0500

    added better logic for rotation and flipping

--- src/day_20.kt
+++ src/day_20.kt
@@ -5,7 +5,7 @@ val tiles = mutableListOf<Tile>()
 
 //flipped: 0 not flipped, 1 left and right flip, 2 top and bottom flip
 data class Tile(
-    val tileNumber: Int, val tileLayout: List<String>,
+    val tileNumber: Int, val tileLayout: MutableList<String>,
     var top: String = "", var bottom: String = "", var right: String = "",
     var left: String = "", var rotated: Int = 0, var flipped: Int = 0
 ) {
@@ -37,53 +37,34 @@ data class Tile(
             println(it)
         }
     }
-
-    fun rotateLayout() {
-        Collections.rotate(tileLayout, 1)
-    }
-
-    //methods to get proper side after transform
-}
-
-data class Tile2(
-    val tileNumber: Int, val tileLayout: List<String>,
-    var top: String = "", var bottom: String = "", var right: String = "",
-    var left: String = "", var rotated: Int = 0, var flipped: Int = 0
-) {
-    fun calcEdges() {
-        top = tileLayout.first()
-        bottom = tileLayout.last()
-        val leftBuilder = StringBuilder()
-        val rightBuilder = StringBuilder()
-        tileLayout.forEach {
-            leftBuilder.append(it.first())
-            rightBuilder.append(it.last())
-        }
-        left = leftBuilder.toString()
-        right = rightBuilder.toString()
-    }
-
-    fun printTile() {
-        tileLayout.forEach {
-            println(it)
+    fun rotateLayout(turns: Int) {
+        (0..turns-1).forEach {
+            rotate()
         }
     }
 
-    fun rotateLayout() {
-        Collections.rotate(tileLayout, 1)
+    fun rotate() {
+        val m = tileLayout.size;
+        val n = tileLayout[0].length;
+
+        val rotatedLayout = mutableListOf<StringBuilder>()
+        rotatedLayout.addAll(tileLayout.map { StringBuilder(it)})
+        (0..m-1).forEachIndexed { ix, x ->
+            (0..n-1).forEachIndexed { iy, y ->
+                rotatedLayout[iy][m-ix-1] = tileLayout[ix][iy]
+            }
+        }
+        tileLayout.clear()
+        tileLayout.addAll(rotatedLayout.map{ it.toString() })
     }
 
-    fun topReversed(): Long = top.reversed().toLong()
-    fun bottomReversed(): Long = bottom.reversed().toLong()
-    fun leftReversed(): Long = left.reversed().toLong()
-    fun rightReversed(): Long = right.reversed().toLong()
+    fun flipHead() {
 
-    fun topL(): Long = top.toLong()
-    fun bottomL(): Long = bottom.toLong()
-    fun leftL(): Long = left.toLong()
-    fun rightL(): Long = right.toLong()
+    }
 
-    //methods to get proper side after transform
+    fun flipSides() {
+
+    }
 }
 
 fun readTiles(fileName: String) {
@@ -232,143 +213,90 @@ fun tilesAssemble(): Long {
 
 //part 2 - my part one solution won't solve the solution. It will give me the corners
 // but that's it. I'll need to build the whole thing.
+
 val combinedImage = mutableListOf<MutableList<Tile?>>()
 var dimensions = 0
 var usedTiles = mutableSetOf<Int>()
-//fun fitTileEdge(topPiece: Long) : Tile {
-//    var top = false
-//    tiles.forEach main@{ it ->
-//        //unmodified
-//        if ((topPiece xor it.topL()) == 0L) top = true
-//        if(top) {
-//            return it
-//        }
-//        //rotated 90
-//        if ((topPiece xor it.leftReversed()) == 0L) top = true
-//        if (top){
-//            it.top =  it.leftReversed().to
-//            return it
-//        }
-//        if ((t.topL() xor it.rightReversed()) == 0L) top = true
-//        if ((t.bottomL() xor it.leftReversed()) == 0L) bottom = true
-//        if ((t.rightL() xor it.bottomL()) == 0L) right = true
-//        if ((t.leftL() xor it.topL()) == 0L) left = true
-//
-//        //rotated 180
-//
-//        //rotated 270
-//
-//        //flipped left right
-//
-//        //flipped left right 90
-//
-//        //flipped left right 180
-//
-//        //flipped left right 270
-//
-//        //flipped top bottom
-//
-//
-//        //flipped top bottom 90
-//
-//
-//        //flipped top bottom 180
-//
-//
-//        //flipped top bottom 270
-//
-//    }
-//}
-//fun fitTile(leftPiece: Long): Tile {
-//    tiles.forEach main@{ it ->
-//        var left = false
-//        //unmodified
-//        if ((topPiece!! xor it.topL()) == 0L) top = true
-//        if ((leftPiece!! xor it.leftL()) == 0L) left = true
-//        if ((t.bottomL() xor it.topL()) == 0L) bottom = true
-//        if ((t.rightL() xor it.leftL()) == 0L) right = true
-//
-//
-//        //rotated 90
-//        if ((t.topL() xor it.rightReversed()) == 0L) top = true
-//        if ((t.bottomL() xor it.leftReversed()) == 0L) bottom = true
-//        if ((t.rightL() xor it.bottomL()) == 0L) right = true
-//        if ((t.leftL() xor it.topL()) == 0L) left = true
-//
-//        //rotated 180
-//        if ((t.topL() xor it.topReversed()) == 0L) top = true
-//        if ((t.bottomL() xor it.bottomReversed()) == 0L) bottom = true
-//        if ((t.rightL() xor it.rightReversed()) == 0L) right = true
-//        if ((t.leftL() xor it.leftReversed()) == 0L) left = true
-//
-//        //flipped left right
-//        if ((t.topL() xor it.bottomReversed()) == 0L) top = true
-//        if ((t.bottomL() xor it.topReversed()) == 0L) bottom = true
-//        if ((t.rightL() xor it.rightL()) == 0L) right = true
-//        if ((t.leftL() xor it.leftL()) == 0L) left = true
-//
-//        //flipped left right 90
-//        if ((t.topL() xor it.leftReversed()) == 0L) top = true
-//        if ((t.bottomL() xor it.rightReversed()) == 0L) bottom = true
-//        if ((t.rightL() xor it.bottomReversed()) == 0L) right = true
-//        if ((t.leftL() xor it.topReversed()) == 0L) left = true
-//
-//        //flipped left right 180
-//        if ((t.topL() xor it.topL()) == 0L) top = true
-//        if ((t.bottomL() xor it.bottomL()) == 0L) bottom = true
-//        if ((t.rightL() xor it.leftReversed()) == 0L) right = true
-//        if ((t.leftL() xor it.rightReversed()) == 0L) left = true
-//
-//        //flipped left right 270
-//        if ((t.topL() xor it.rightL()) == 0L) top = true
-//        if ((t.bottomL() xor it.leftL()) == 0L) bottom = true
-//        if ((t.rightL() xor it.topL()) == 0L) right = true
-//        if ((t.leftL() xor it.bottomL()) == 0L) left = true
-//
-//        //flipped top bottom
-//        if ((t.topL() xor it.topReversed()) == 0L) top = true
-//        if ((t.bottomL() xor it.bottomReversed()) == 0L) bottom = true
-//        if ((t.rightL() xor it.leftL()) == 0L) right = true
-//        if ((t.leftL() xor it.rightL()) == 0L) left = true
-//
-//        //flipped top bottom 90
-//        if ((t.topL() xor it.rightReversed()) == 0L) top = true
-//        if ((t.bottomL() xor it.leftReversed()) == 0L) bottom = true
-//        if ((t.rightL() xor it.topReversed()) == 0L) right = true
-//        if ((t.leftL() xor it.bottomReversed()) == 0L) left = true
-//
-//        //flipped top bottom 180
-//        if ((t.topL() xor it.bottomL()) == 0L) top = true
-//        if ((t.bottomL() xor it.topL()) == 0L) bottom = true
-//        if ((t.rightL() xor it.rightReversed()) == 0L) right = true
-//        if ((t.leftL() xor it.leftReversed()) == 0L) left = true
-//
-//        //flipped top bottom 270
-//        if ((t.topL() xor it.leftL()) == 0L) top = true
-//        if ((t.bottomL() xor it.rightL()) == 0L) bottom = true
-//        if ((t.rightL() xor it.bottomL()) == 0L) right = true
-//        if ((t.leftL() xor it.topL()) == 0L) left = true
-//
-//
-//        if (top && left && !right && !bottom) {
-//            combinedImage[0][0] = t
-//            println("Tile: ${t.tileNumber}")
-//        }
-//        if (top && right && !left && !bottom) {
-//            r)
-//            combinedImage[0][dimensions - 1] = t
-//            println("Tile: ${t.tileNumber}")
-//        }
-//        if (bottom && left && !top && !right) {
-//            combinedImage[dimensions - 1][0] = t
-//            println("Tile: ${t.tileNumber}")
-//        }
-//        if (bottom && right && !top && !left) {
-//            combinedImage[dimensions - 1][dimensions - 1] = t
-//            println("Tile: ${t.tileNumber}")
-//        }
-//    }
-//}
+
+fun fitTileTop(piece: Long) : Tile {
+    var top = false
+    tiles.forEach main@{ it ->
+
+        (1..3).forEach {i ->
+            if ((piece xor it.leftL()) == 0L) top = true
+            if(top) {
+                return it
+            }
+            it.rotate()
+        }
+
+        it.flipSides()
+        (1..4).forEach {i ->
+            if ((piece xor it.leftL()) == 0L) top = true
+            if(top) {
+                return it
+            }
+            it.rotate()
+        }
+
+        //reset
+        it.flipSides()
+
+        it.flipHead()
+        (1..4).forEach {i ->
+            if ((piece xor it.leftL()) == 0L) top = true
+            if(top) {
+                return it
+            }
+            it.rotate()
+        }
+
+        //reset
+        it.flipHead()
+    }
+    //shouldn't come to this
+    return Tile(0, mutableListOf("000"))
+}
+
+fun fitTile(piece: Long) : Tile {
+    var top = false
+    tiles.forEach main@{ it ->
+
+        (1..3).forEach {i ->
+            if ((piece xor it.topL()) == 0L) top = true
+            if(top) {
+                return it
+            }
+            it.rotate()
+        }
+
+        it.flipSides()
+        (1..4).forEach {i ->
+            if ((piece xor it.topL()) == 0L) top = true
+            if(top) {
+                return it
+            }
+            it.rotate()
+        }
+
+        //reset
+        it.flipSides()
+
+        it.flipHead()
+        (1..4).forEach {i ->
+            if ((piece xor it.topL()) == 0L) top = true
+            if(top) {
+                return it
+            }
+            it.rotate()
+        }
+
+        //reset
+        it.flipHead()
+    }
+    //shouldn't come to this
+    return Tile(0, mutableListOf("000"))
+}
 
 fun initCombinedImage() {
     (0..dimensions - 1).forEach { x ->
@@ -399,8 +327,4 @@ fun main() {
     println(tiles.count())
     println(tiles)
     println("All the corners = ${tilesAssemble()}")
-    tiles[0].printTile()
-    tiles[0].rotateLayout()
-    println("\n")
-    tiles[0].printTile()
 }
